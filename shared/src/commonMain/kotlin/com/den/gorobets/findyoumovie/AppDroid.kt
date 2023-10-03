b4e65de069a47b0c222061577d4563d0f68cdb42
denisgorobec <denis.d.30.04.2997@gmail.com>
2023-10-03 23:03:54 +0300

    Add screens and navigation

--- shared/src/commonMain/kotlin/com/den/gorobets/findyoumovie/AppDroid.kt
+++ shared/src/commonMain/kotlin/com/den/gorobets/findyoumovie/AppDroid.kt
@@ -2,7 +2,7 @@ package com.den.gorobets.findyoumovie
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import com.den.gorobets.findyoumovie.ui.DiscoveryScreen
+import com.den.gorobets.findyoumovie.ui.screen.DiscoveryScreen
 
 @Composable
 fun App() {
---
+++ shared/src/commonMain/kotlin/com/den/gorobets/findyoumovie/ui/screen/SerialDescriptionScreen.kt
@@ -0,0 +1,15 @@
+package com.den.gorobets.findyoumovie.ui.screen
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class SerialDescriptionScreen : Screen {
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
+    }
+}
